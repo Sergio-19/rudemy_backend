747019c7c872ccc448e88f72f5ee17213dc71538
unknown <you@example.com>
2022-07-06 12:06:12 +0300

    изменил конфиг перед заливкой на сервер

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    port: "5000",
-    // host: 'stended3.beget.tech',
-    // user: 'stended3_rudemy',
-    // password: 'Se549297',
-    // database: 'stended3_rudemy'
+    port: "8080",
+    host: 'stended3.beget.tech',
+    user: 'stended3_rudemy',
+    password: 'Se549297',
+    database: 'stended3_rudemy'
         
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'rudemy_project'
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'root',
+    // database: 'rudemy_project'
 }
